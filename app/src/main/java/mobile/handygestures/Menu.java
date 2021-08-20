9a34b65734af7d0b6c617a76362c436969835fa4
JoelKanyi <joelkanyi98@gmail.com>
2021-08-20 15:03:45 +0300

    changed color, added sign in, sign up and options menu

--- app/src/main/java/mobile/handygestures/Menu.java
+++ app/src/main/java/mobile/handygestures/Menu.java
@@ -3,11 +3,14 @@ package mobile.handygestures;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class Menu extends AppCompatActivity {
-// The menu activity handles the start menu with buttons to other activities and the quit option. This is the starting activity.
+    // The menu activity handles the start menu with buttons to other activities and the quit option. This is the starting activity.
     Button galleryButton;
     Button helpButton;
     Button quitButton;
@@ -59,5 +62,21 @@ public class Menu extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(android.view.Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (R.id.item1 == item.getItemId()) {
+            Toast.makeText(this, "Item I Clicked", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
